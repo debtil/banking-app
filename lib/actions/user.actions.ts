c3c9ee18d6eea26f799fd6a7b0a73d8d393132a8
debtil <matheus.debtilsouza@gmail.com>
2024-09-06 16:22:14 -0300

    create user with appwrite is working

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,17 +1,48 @@
-'use client';
+'use server';
+
+import { parseStringify } from "../utils";
+import { createSessionClient, createAdminClient } from '@/lib/server/appwrite';
+import { ID } from "node-appwrite";
+import { cookies } from "next/headers";
 
 export const signIn = async () => {
-    try{
+  try {
 
-    }catch(error){
-        console.log(error);
-    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export const signUp = async (userData: SignUpParams) => {
-    try{
+  const { email, password, firstName, lastName } = userData
 
-    }catch(error){
-        console.log(error);
-    }
+  try {
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(ID.unique(), email, password, `${firstName} ${lastName}`);
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true
+    });
+
+    return parseStringify(newUserAccount);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    console.log(error)
+    return null;
+  }
 }
